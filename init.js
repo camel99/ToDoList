3dbff3d56321103a8adbe364269a3c95845340e9
Maciej Kamela <maciej.kamela@pearson.com>
2015-07-07 22:57:13 +0200

    Adding local storage file

--- init.js
+++ init.js
@@ -1,6 +1,8 @@
 window.onload = function () {
   var newTask = document.getElementById("newTaskEntry"),
-      table = document.getElementById("outcomeTable");
+      table = document.getElementById("outcomeTable"),
+      value = document.querySelectorAll('.taskOutcome'),
+      name = 'task';
 
   newEntry.startTime();// first run to be sure that there is no delay in current time
   setInterval (function(){
